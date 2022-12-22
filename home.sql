8a5301f01c252f067a39beb8318224f954326028
Dedeepya Yarra <dedeepya.dd@gmail.com>
2022-12-22 23:20:17 +0530

    Add files via upload

---
+++ home.sql
@@ -0,0 +1,84 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 22, 2022 at 06:49 PM
+-- Server version: 10.4.10-MariaDB
+-- PHP Version: 7.3.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `home`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kitadd`
+--
+-- Error reading structure for table home.kitadd: #1932 - Table 'home.kitadd' doesn't exist in engine
+-- Error reading data for table home.kitadd: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `home`.`kitadd`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kititems`
+--
+-- Error reading structure for table home.kititems: #1932 - Table 'home.kititems' doesn't exist in engine
+-- Error reading data for table home.kititems: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `home`.`kititems`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kitquant`
+--
+-- Error reading structure for table home.kitquant: #1932 - Table 'home.kitquant' doesn't exist in engine
+-- Error reading data for table home.kitquant: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `home`.`kitquant`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kitrating`
+--
+-- Error reading structure for table home.kitrating: #1932 - Table 'home.kitrating' doesn't exist in engine
+-- Error reading data for table home.kitrating: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `home`.`kitrating`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kitspecials`
+--
+-- Error reading structure for table home.kitspecials: #1932 - Table 'home.kitspecials' doesn't exist in engine
+-- Error reading data for table home.kitspecials: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `home`.`kitspecials`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `phonecode`
+--
+-- Error reading structure for table home.phonecode: #1932 - Table 'home.phonecode' doesn't exist in engine
+-- Error reading data for table home.phonecode: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `home`.`phonecode`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `register`
+--
+-- Error reading structure for table home.register: #1932 - Table 'home.register' doesn't exist in engine
+-- Error reading data for table home.register: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `home`.`register`' at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
